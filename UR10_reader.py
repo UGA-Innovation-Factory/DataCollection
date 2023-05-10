f5c23f5b0b502384d99eb022b9e400a611ab9979
Tomás L <37879596+Tomz295@users.noreply.github.com>
2023-05-10 16:13:43 -0400

    ur10 publishes as single json -some old code cleanup

--- UR10_reader.py
+++ UR10_reader.py
@@ -1,42 +1,20 @@
-import time, sys
+import time, sys, json
 from HiveMQConnect import HiveClient
+
 from rtde.rtde_ur10_connection import UR10Listener
+#rtde from: https://github.com/UniversalRobots/RTDE_Python_Client_Library
 
-def fakeDataGen():
-    while True:
-        with open("UR10Output.txt", "r") as file:
-            while True:
-                text = file.readline()
-                if not text:
-                    break
-                yield " " + text
-
-
-#iterator = None
-# def begin_connection():
-#     socket_inst = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     host = "172.22.114.160"            # Ip addres of the robot
-#     port = 30002                     # Portnr of the robot
-#     socket_inst.settimeout(0.1)
-
-#     try:
-#         socket_inst.connect((host, port))
-#         new_angle_reader = UR10_reader(host)
-#     except OSError as error:
-#         print("UR10 Connection OS error: {0}".format(error))
-#         raise OSError(error)
-#         #print("~ Sending fake data")
-#         #return (lambda : (271, 249, 257, 305, 90, 0, 1,2,3,4,5,6, 11,12,13,14,15,16)), 0.5
-#         #iterator = fakeDataGen()
-#         #return (lambda : next(iterator)), 0.07
-
-#     socket_inst.sendall(b"set_digital_out(8, False)"+ b"\n") #self.r_disable_magnet()
-#     print("Connected")
-#     return new_angle_reader.get_UR_info, 0.01  
+# def fakeDataGen():
+#     while True:
+#         with open("UR10Output.txt", "r") as file:
+#             while True:
+#                 text = file.readline()
+#                 if not text:
+#                    break
+#                yield " " + text
 
 
 if __name__ == '__main__':
-    #getter, time_out = begin_connection()
     print("Starting up...")
 
     UR10_ip = "172.22.114.160"
@@ -55,8 +33,8 @@ if __name__ == '__main__':
         while True:
             time.sleep(time_out)
             message_dict = ur10_connection.read_dict()
-            #print(message)
-            hqt_client.publish_dict("DT/UR10", message_dict)
+            #print(message_dict)
+            hqt_client.publish("DT/UR10", json.dumps(message_dict))
     except KeyboardInterrupt:
         print("Stopping...")
         hqt_client.loop_stop_and_disconnect()
@@ -67,7 +45,7 @@ if __name__ == '__main__':
 
 
 
-
+    # Old code - ignore
     # while True:
     #     try:
     #         print("\nTrying to connect to Unity.")
